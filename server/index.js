f1faed40ad5c206d87331e8f795d03f1073dbe23
root <root@vultr.guest>
2024-07-03 06:49:26 +0000

    update cors

--- server/index.js
+++ server/index.js
@@ -16,11 +16,7 @@ socket.connect(server);
 
 
 
-app.use(cors({
-    origin: ['http://localhost:3000'],
-    credentials: true,
-
-}));
+app.use(cors());
 app.use(express.json())
 
 app.use('/api', customer_router);
